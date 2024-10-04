a0207a8740afbb5272639558e1829ea2aa4e4cd1
pitagora <pitagora12095@gmail.com>
2024-10-04 22:07:08 +0900

    Add files via upload

---
+++ Fizz Buzz 2CEM1140.py
@@ -0,0 +1,14 @@
+import math
+
+x = int(input("文字入力"))
+print()
+
+for x in range(1, x+1):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        print(x)
